1bbbd720f29b82b6868b00eb1d74b5c707e88cb4
mattretterer <matthew.m.retterer@parthenon.ey.com>
2025-05-04 18:25:29 -0400

    16_PowerDigit

--- 15_lattice_paths.py
+++ 15_lattice_paths.py
@@ -7,20 +7,24 @@ ONLY move DOWN or RIGHT
 
 """
 
+# This solution is O(mn) time complexity where m is the number of rows and n is the number of columns. With memoization nothing is visited more than once
 class Solution:
     def LatticePathCount(self, max_columns: int, max_rows: int) -> int:
 
         # memoize results to reduce calcuating the same path over and over again
         path = {}
+        # use recursion to explore all viable paths
         count = self.RecurPaths(0,0, max_columns, max_rows,path)
 
         return count
 
 
     def RecurPaths(self, col_position: int, row_position: int, max_columns: int, max_rows: int, path: dict) -> int:
-
+        
+        # Instantiate a count variable
         count = 0
         
+        # If a position has already been visited....then simply return however many unique paths go from that position to the bottom right
         if (col_position, row_position) in path:
             return path[(col_position, row_position)]
 
@@ -32,12 +36,15 @@ class Solution:
  
         # Go Down
         if row_position + 1 <= max_rows:
+            # Sum up all the counts as the recusion comes back
             count += self.RecurPaths(col_position, row_position+1, max_columns, max_rows,path)
         # Go Right
         if col_position + 1 <= max_columns:
+            # Sum up all the counts as the recusion comes back
             count += self.RecurPaths(col_position+1, row_position, max_columns, max_rows,path)
 
-
+        # As the recursion comes back and each individual row, col is revisisted, the final total count for that position is listed
+        # Ultimately all the counts collate back till (0,0) which is then the sum
         path[(col_position, row_position)] = count
         return count
 
