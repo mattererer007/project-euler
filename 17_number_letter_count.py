45e9420995801859d03f7643f127f70235eada5e
mattretterer <matthew.m.retterer@parthenon.ey.com>
2025-05-06 16:18:31 -0400

    tiny changes made

--- 17_number_letter_count.py
+++ 17_number_letter_count.py
@@ -36,25 +36,36 @@ class Solution:
 
         letter_number = ""
 
+        # If the number is in the thousands and there are numbers > 0 in the 
+        # hundreds, tens, or ones place...add 'and'
         if x // 1000 > 0 and (x % 1000 > 0):
             letter_number += "and"
+        # Else if the nunber is in the hundreds and there are numbers in the tens or ones > 0
+        # ...add 'and'
         elif x // 100 > 0 and (x % 100 > 0):
             letter_number += "and"
         
+        # if number is in thousands, add 'thousand' and then determine in what thousands place it is in 1,2,3,4...
         if x // 1000 > 0:
             letter_number += self.thousands
+            # pull just the digit in the thousands place
             letter_number += self.numbers[x // 1000]
 
+        # If there is a number in the hundreds place and it is less than 10 (special case handled by next...)
         if 0 < (x // 100) % 100 < 10:
             letter_number += self.hundreds
             letter_number += self.numbers[(x // 100) % 100]
         
+        # If there is a number in the tens place...
         if (x // 10) % 10 > 0:
+            # if it is 1 then it must be between 10 and 19....
             if (x // 10) % 10  == 1:
                 letter_number += self.teens[x % 100]
+            # Else it is is 20-90
             else:
                 letter_number += self.tens[(x // 10) % 10]
 
+        # Check what the 1s digit is and andd it 
         if x % 10 > 0 and (x // 10) % 10 != 1:
             letter_number += self.numbers[x % 10]
 
